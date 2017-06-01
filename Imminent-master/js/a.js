f2c86a44fa63dda267d7267eecad88a9dbb9e982
prasathth <prasathth.bm.mail@gmail.com>
2017-06-01 17:19:19 +0530

    Add files via upload

---
+++ Imminent-master/js/a.js
@@ -0,0 +1,10 @@
+setTimeout(function(){
+$(".element0").typed({
+    strings: ["apt-get install konquer javascript"],
+    typeSpeed: 10, // typing speed
+    backDelay: 750, // pause before backspacing
+    loop: false, // loop on or off (true or false)
+    loopCount: false, // number of loops, false = infinite
+    callback: function(){ } // call function after typing is done
+    });
+  }, 0);
